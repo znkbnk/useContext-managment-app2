60c72167b36d7b0e6cd4e30a5f50ae34d1963573
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-06-21 20:29:26 +0100

    Create UserProvider.js

---
+++ UserProvider.js
@@ -0,0 +1,19 @@
+  //UserProvider.js
+import React, { useState } from 'react';
+import UserContext from './UserContext';
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const updateUser = (userData) => {
+    setUser(userData);
+  };
+
+  return (
+    <UserContext.Provider value={{ user, updateUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
